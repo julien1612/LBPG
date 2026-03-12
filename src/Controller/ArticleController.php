932cd0a3979f845f9fc962b968127da78695c68d
julien1612 <cjulien1612@gmail.com>
2026-03-12 22:38:08 +0100

    article/pagination ok

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,21 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Repository\ArticleRepository;
 
 final class ArticleController extends AbstractController
 {
     #[Route('/article', name: 'app_article')]
     public function index(
-        ArticleRepository $articleRepository
+        ArticleRepository $articleRepository,
+        PaginatorInterface $paginator,
+        Request $request
     ): Response {
 
-        $articles = $articleRepository->findBy([], ['createdAt' => 'DESC']);
+        $data = $articleRepository->findBy([], ['createdAt' => 'DESC']);
+
+        $articles = $paginator->paginate(
+            $data, 
+            $request->query->getInt('page', 1), 
+            5
+        );
+
         return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
             'articles' => $articles
         ]);
     }
