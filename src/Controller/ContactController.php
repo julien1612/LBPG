c99cf094baacd953df95983285a9f9640797df7d
julien1612 <cjulien1612@gmail.com>
2026-03-14 00:05:22 +0100

    contact/form/email ok

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,51 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ContactController extends AbstractController
-{
+{ // <--- Il manquait cette accolade pour ouvrir la classe
+
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // On récupère les données (sous forme de tableau si non lié à une entité)
+            $contactData = $form->getData();
+
+            // Création de l'e-mail
+            $email = (new TemplatedEmail())
+                ->from('noreply@tonsite.fr') // CONSEIL : Utilise une adresse de ton domaine ici
+                ->replyTo($contactData['email']) // Et mets l'email du visiteur ici pour pouvoir répondre
+                ->to('contact@trait-d-union.fr')
+                ->subject('Nouveau message de contact : ' . $contactData['nom'])
+                ->htmlTemplate('emails/contact_email.html.twig')
+                ->context([
+                    'prenom' => $contactData['prenom'],
+                    'nom' => $contactData['nom'],
+                    'mail_visiteur' => $contactData['email'],
+                    'message' => $contactData['message'],
+                    'statut' => $contactData['vous_etes'],
+                ]);
+
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Votre message a bien été envoyé à l\'association !');
+
+            return $this->redirectToRoute('app_contact');
+        }
+
         return $this->render('contact/contact.html.twig', [
-            'controller_name' => 'ContactController',
+            'contactForm' => $form->createView(), // Ou juste $form si tu es sur une version récente de Symfony
         ]);
     }
-}
+} // <--- Il manquait cette accolade pour fermer la classe
---
+++ src/Form/ContactType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ContactType extends AbstractType
+{
+    // ... imports (TextType, EmailType, ChoiceType, TextareaType...)
+
+public function buildForm(FormBuilderInterface $builder, array $options): void
+{
+    $builder
+        ->add('prenom', TextType::class, [
+            'label' => 'Prénom *',
+            'attr' => ['placeholder' => 'Saisissez votre prénom']
+        ])
+        ->add('nom', TextType::class, [
+            'label' => 'Nom *',
+            'attr' => ['placeholder' => 'Saisissez votre nom']
+        ])
+        ->add('email', EmailType::class, [
+            'label' => 'Email *',
+            'attr' => ['placeholder' => 'Saisissez votre adresse mail']
+        ])
+        ->add('vous_etes', ChoiceType::class, [
+            'label' => 'Vous êtes *',
+            'choices' => [
+                'Un particulier' => 'particulier',
+                'Une structure' => 'structure',
+                'Un bénévole' => 'benevole',
+            ],
+            'placeholder' => 'Vous êtes',
+        ])
+        ->add('message', TextareaType::class, [
+            'label' => 'Message *',
+            'attr' => ['placeholder' => 'Saisissez votre message', 'rows' => 5]
+        ]);
+}
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
