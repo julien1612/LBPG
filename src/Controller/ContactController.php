f17c8b501b14a219f28073b48303fa8082175272
julien1612 <cjulien1612@gmail.com>
2026-03-14 10:20:49 +0100

    page contact ok

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ContactController extends AbstractController
-{ // <--- Il manquait cette accolade pour ouvrir la classe
+{ 
 
     #[Route('/contact', name: 'app_contact')]
     public function index(Request $request, MailerInterface $mailer): Response
@@ -20,13 +20,11 @@ final class ContactController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // On récupère les données (sous forme de tableau si non lié à une entité)
             $contactData = $form->getData();
 
-            // Création de l'e-mail
             $email = (new TemplatedEmail())
                 ->from('noreply@tonsite.fr') // CONSEIL : Utilise une adresse de ton domaine ici
-                ->replyTo($contactData['email']) // Et mets l'email du visiteur ici pour pouvoir répondre
+                ->replyTo($contactData['email']) 
                 ->to('contact@trait-d-union.fr')
                 ->subject('Nouveau message de contact : ' . $contactData['nom'])
                 ->htmlTemplate('emails/contact_email.html.twig')
@@ -46,7 +44,7 @@ final class ContactController extends AbstractController
         }
 
         return $this->render('contact/contact.html.twig', [
-            'contactForm' => $form->createView(), // Ou juste $form si tu es sur une version récente de Symfony
+            'contactForm' => $form->createView(), 
         ]);
     }
-} // <--- Il manquait cette accolade pour fermer la classe
+} 
